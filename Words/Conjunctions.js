edddf32ba23c4994649827939a4a46c925988dd8
Maxi Mittelhammer <Maxi@Maximilians-Air.fritz.box>
2016-10-04 19:02:39 +0200

    Alpha 0.0.1

---
+++ Words/Conjunctions.js
@@ -0,0 +1,10 @@
+/**
+ Export to JSON plugin for PHPMyAdmin
+ @version 4.6.3
+ */
+
+// Database 'Shorter'
+
+// Shorter.entries
+
+var conj = [{"word":"Another"}, {"word":"Another"}, {"word":"Another"}, {"word":"Cestuy"}, {"word":"Cestui"}, {"word":"Echon"}, {"word":"Echoon"}, {"word":"Everichon"}, {"word":"Everychon"}, {"word":"Hem"}, {"word":"Hemself"}, {"word":"Hemselven"}, {"word":"Her"}, {"word":"Here"}, {"word":"Here"}, {"word":"Hers"}, {"word":"Herself"}, {"word":"Herself"}, {"word":"Him"}, {"word":"Him"}, {"word":"Himself"}, {"word":"Himself"}, {"word":"Himselve"}, {"word":"Hir"}, {"word":"Hire"}, {"word":"Hires"}, {"word":"Hirs"}, {"word":"His"}, {"word":"His"}, {"word":"Hit"}, {"word":"Ho"}, {"word":"Ich"}, {"word":"Ik"}, {"word":"Ilkon"}, {"word":"Ilkoon"}, {"word":"It"}, {"word":"It"}, {"word":"It"}, {"word":"It"}, {"word":"It"}, {"word":"It"}, {"word":"Itself"}, {"word":"Me"}, {"word":"Men"}, {"word":"Mine"}, {"word":"Myself"}, {"word":"Myselven"}, {"word":"Oneself"}, {"word":"Other"}, {"word":"Other"}, {"word":"Other"}, {"word":"Other"}, {"word":"Ourselves"}, {"word":"Thee"}, {"word":"Their"}, {"word":"Them"}, {"word":"Themselves"}, {"word":"There"}, {"word":"There"}, {"word":"There"}, {"word":"These"}, {"word":"Thilk"}, {"word":"Thine"}, {"word":"This"}, {"word":"This"}, {"word":"Those"}, {"word":"Thy"}, {"word":"Thyself"}, {"word":"Us"}, {"word":"Whate'er"}, {"word":"Whatever"}, {"word":"Whatsoe'er"}, {"word":"Whatsoever"}, {"word":"Whether"}, {"word":"Which"}, {"word":"Which"}, {"word":"Whichever"}, {"word":"Whichsoever"}, {"word":"Whilk"}, {"word":"Who"}, {"word":"Whoever"}, {"word":"Whom"}, {"word":"Whomsoever"}, {"word":"Whose"}, {"word":"Whosesoever"}, {"word":"Whoso"}, {"word":"Whosoever"}, {"word":"Y"}, {"word":"Ye"}, {"word":"Your"}, {"word":"Yours"}, {"word":"Yourself"}, {"word":"Yow"}];
